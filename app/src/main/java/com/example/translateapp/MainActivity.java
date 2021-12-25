10e76aa09528e3f4724c21f3403a0f660c2f2f27
reyhanemaleki <reyhane.maleki56@gmail.com>
2021-12-25 18:10:47 +0330

    design splash screen ui

---
+++ app/src/main/java/com/example/translateapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.translateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(() -> {
+            Intent splash = new Intent(MainActivity.this,
+                    Second.class);
+            startActivity(splash);
+            finish();
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
